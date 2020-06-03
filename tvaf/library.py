24e91d4d332e873d39428cbaf9255372c130fa00
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2020-06-03 00:46:12 +0000

    Add content library.

---
+++ tvaf/library.py
@@ -0,0 +1,253 @@
+"""
+/v1/{info_hash}/{tracker}/f/{path} -> ../../../i/{index}
+/v1/{info_hash}/{tracker}/i/{index}
+/v1/{info_hash}/default -> btn  # later
+/browse/{library}/{meaningful path} -> ../../../v1/{info_hash}
+
+btn, ptp: given torrent info_hash, access torrent data
+default: given torrent info_hash, and seeder counts, redirects to concrete
+accessors
+cross-tracker: given file hash, downloads from multiple concrete trackers
+"""
+from __future__ import annotations
+
+import dataclasses
+import pathlib
+from typing import Optional
+from typing import Callable
+import logging
+from tvaf import protocol
+from typing import List
+from typing import Dict
+from typing import Iterator
+import stat as stat_lib
+from tvaf import config as config_lib
+from typing import Any
+from typing import cast
+from tvaf import fs
+from tvaf import util
+
+
+_log = logging.getLogger(__name__)
+
+Path = pathlib.PurePosixPath
+
+GetTorrent = Callable[[], bytes]
+
+
+@dataclasses.dataclass
+class Hints:
+
+    mime_type: Optional[str] = None
+    content_encoding: Optional[str] = None
+    mtime: Optional[int] = None
+    filename: Optional[str] = None
+
+    def merge(self, other:Hints):
+        if self.mime_type is None:
+            self.mime_type = other.mime_type
+        if self.content_encoding is None:
+            self.content_encoding = other.content_encoding
+        if self.filename is None:
+            self.filename = other.filename
+        if self.mtime is None or (other.mtime is not None and other.mtime <
+                self.mtime):
+            self.mtime = other.mtime
+
+
+class TorrentFile(fs.File):
+
+    def __init__(self, *, info_hash:str=None, start:int=None, stop:int=None,
+            get_torrent:GetTorrent=None, hints:Hints=None):
+        assert info_hash is not None
+        assert start is not None
+        assert stop is not None
+        assert get_torrent is not None
+        assert hints is not None
+        super().__init__(size=stop - start, mtime=hints.mtime)
+        self.info_hash = info_hash
+        self.start = start
+        self.stop = stop
+        self.get_torrent = get_torrent
+        self.hints = hints
+
+
+def _is_valid_path(path:List[str]):
+    for part in path:
+        if "/" in part:
+            return False
+        if part in (".", ".."):
+            return False
+    return True
+
+
+class _V1TorrentAccess(fs.StaticDir):
+
+    def __init__(self, libs:LibraryService, info_hash:str, info:protocol.Info, access:Access):
+        assert access.redirect_to is None
+        super().__init__()
+
+        self._by_path = fs.StaticDir()
+        self.mkchild("f", self._by_path)
+        self._by_index = fs.StaticDir()
+        self.mkchild("i", self._by_index)
+
+        for spec in info.iter_files():
+            self._add_torrent_file(libs, info_hash, spec, access)
+
+    def _add_torrent_file(self, libs:LibraryService, info_hash:str, spec:protocol.FileSpec,
+            access:Access):
+        if spec.is_pad:
+            return
+        if spec.is_symlink:
+            # TODO
+            return
+
+        hints = Hints()
+        for name, func in libs.get_hints_funcs.items():
+            try:
+                hints.merge(func(info_hash, spec.index))
+            except KeyError:
+                pass
+            except Exception as e:
+                _log.exception("%s: get_hints(%s, %s)", name, info_hash,
+                        spec.index)
+        hints.filename = spec.full_path[-1]
+        torrent_file = TorrentFile(info_hash=info_hash, start=spec.start,
+            stop=spec.stop, get_torrent=access.get_torrent, hints=hints)
+        self._by_index.mkchild(str(spec.index), torrent_file)
+
+        if not _is_valid_path(spec.full_path):
+            return
+
+        parent = self._try_mkdirs(spec.full_path[:-1])
+        if not parent:
+            return
+        parent.mkchild(spec.full_path[-1], fs.Symlink(target=torrent_file))
+
+    def _try_mkdirs(self, dirnames:List[str]) -> Optional[fs.StaticDir]:
+        parent = self._by_path
+        for name in dirnames:
+            child = parent.children.get(name)
+            if not child:
+                child = fs.StaticDir()
+                parent.mkchild(name, child)
+            elif not isinstance(child, fs.StaticDir):
+                return None
+            parent = child
+        return parent
+
+
+class _V1Torrent(fs.Dir):
+
+    def __init__(self, libs:LibraryService, info_hash:str, info:protocol.Info):
+        super().__init__()
+        self.libs = libs
+        self.info_hash = info_hash
+        self.info = info
+
+    def _get_one_access(self, info_hash:str, accessor_name:str) -> Optional[Access]:
+        func = self.libs.get_access_funcs.get(accessor_name)
+        if not func:
+            return None
+
+        try:
+            return func(info_hash)
+        except KeyError:
+            pass
+        except Exception:
+            _log.exception("%s: get_access(%s)", accessor_name, info_hash)
+        return None
+
+    def get_node(self, name:str) -> Optional[fs.Node]:
+        access = self._get_one_access(self.info_hash, name)
+        if not access:
+            return None
+        if access.redirect_to:
+            return fs.Symlink(target=access.redirect_to)
+        else:
+            return _V1TorrentAccess(self.libs, self.info_hash, self.info,
+                    access)
+
+    def readdir(self) -> Iterator[fs.Dirent]:
+        for name in self.libs.get_access_funcs:
+            access = self._get_one_access(self.info_hash, name)
+            if not access:
+                continue
+            if access.redirect_to:
+                yield fs.Dirent(name=name,
+                        stat=fs.Stat(filetype=stat_lib.S_IFLNK))
+            else:
+                yield fs.Dirent(name=name,
+                        stat=fs.Stat(filetype=stat_lib.S_IFDIR))
+
+
+class _V1(fs.Dir):
+
+    def __init__(self, libs:LibraryService):
+        super().__init__()
+        self.libs = libs
+
+    def get_node(self, info_hash: str) -> Optional[fs.Node]:
+        info_dict = None
+        for name, func in self.libs.get_layout_info_dict_funcs.items():
+            try:
+                info_dict = func(info_hash)
+                break
+            except KeyError:
+                pass
+            except Exception as e:
+                _log.exception("%s: get_layout_info_dict(%s)", name, info_hash)
+        if not info_dict:
+            return None
+        return _V1Torrent(self.libs, info_hash, protocol.Info(info_dict))
+
+
+class _Browse(fs.DictDir):
+
+    def __init__(self, libs:LibraryService):
+        super().__init__()
+        self.libs = libs
+
+    def get_dict(self):
+        return self.libs.browse_nodes
+
+
+class _Root(fs.StaticDir):
+
+    def __init__(self, libs:LibraryService):
+        super().__init__()
+        self.mkchild("v1", _V1(libs))
+        self.mkchild("browse", _Browse(libs))
+
+
+@dataclasses.dataclass
+class Access:
+
+    redirect_to :Optional[str] = None
+    seeders: Optional[int] = None
+    get_torrent: Optional[GetTorrent] = None
+
+    def __post_init__(self):
+        assert (self.redirect_to is not None) ^ (self.get_torrent is not None)
+
+
+GetAccess = Callable[[str], Access]
+GetBDict = Callable[[str], protocol.BDict]
+GetHints = Callable[[str, int], Hints]
+
+class LibraryService:
+
+    def __init__(self):
+        self.root = _Root(libs=self)
+        self.get_access_funcs:Dict[str, GetAccess] = {}
+        self.get_layout_info_dict_funcs:Dict[str, GetBDict] = {}
+        self.get_hints_funcs:Dict[str, GetHints] = {}
+        self.browse_nodes : Dict[str, fs.Node] = {}
+
+    @staticmethod
+    def get_torrent_path(info_hash:str) -> Path:
+        return Path().joinpath("v1", info_hash)
+
+    def lookup_torrent(self, info_hash:str) -> fs.Node:
+        return fs.lookup(self.root, self.get_torrent_path(info_hash))
---
+++ tvaf/tests/library_test.py
@@ -0,0 +1,399 @@
+import unittest
+import stat as stat_lib
+from typing import Iterable
+from typing import Tuple
+from typing import Optional
+from typing import cast
+from . import tdummy
+from tvaf import library
+from typing import Union
+import libtorrent as lt
+from tvaf import fs
+from tvaf import protocol
+from typing import Dict
+
+
+SINGLE = tdummy.Torrent.single_file(name=b"test.txt", length=16384 * 9 + 1000)
+MULTI = tdummy.Torrent(files=[
+    dict(length=10000, path=b"multi/file.tar.gz"),
+    dict(length=100, path=b"multi/info.nfo"),
+])
+PADDED = tdummy.Torrent(files=[
+    dict(length=10000, path=b"padded/file.tar.gz"),
+    dict(length=6384, path=b"padded/.pad/6834", attr=b"p"),
+    dict(length=100, path=b"padded/info.nfo"),
+])
+CONFLICT_FILE = tdummy.Torrent(files=[
+    dict(length=100, path=b"conflict/file.zip"),
+    dict(length=200, path=b"conflict/file.zip"),
+])
+CONFLICT_FILE_DIR = tdummy.Torrent(files=[
+    dict(length=100, path=b"conflict/path/file.zip"),
+    dict(length=200, path=b"conflict/path"),
+])
+CONFLICT_DIR_FILE = tdummy.Torrent(files=[
+    dict(length=100, path=b"conflict/path"),
+    dict(length=200, path=b"conflict/path/file.zip"),
+])
+BAD_PATHS = tdummy.Torrent(files=[
+    dict(length=10, path=b"bad/./file"),
+    dict(length=20, path=b"bad/../file"),
+    dict(length=30, path_split=[b"bad", b"slash/slash", b"file"]),
+])
+
+
+class TestLibraryService(unittest.TestCase):
+
+    def setUp(self):
+        self.torrents = {t.infohash: t for t in (SINGLE, MULTI, PADDED,
+            CONFLICT_FILE, CONFLICT_FILE_DIR, CONFLICT_DIR_FILE, BAD_PATHS)}
+        self.hints = {
+            (SINGLE.infohash, 0): library.Hints(mime_type="text/plain"),
+            (MULTI.infohash, 0): library.Hints(mime_type="application/x-tar",
+                content_encoding="gzip"),
+            (MULTI.infohash, 1): library.Hints(mime_type="text/plain"),
+        }
+
+        self.libs = library.LibraryService()
+        self.libs.get_layout_info_dict_funcs["test"] = self.get_layout_info_dict
+        self.libs.get_hints_funcs["test"] = self.get_hints
+        self.libs.get_access_funcs["test"] = self.get_access
+
+    def get_layout_info_dict(self, info_hash):
+        return self.torrents[info_hash].info
+
+    def get_hints(self, info_hash, index):
+        return self.hints[(info_hash, index)]
+
+    def get_access(self, info_hash):
+        t = self.torrents[info_hash]
+        return library.Access(seeders=100, get_torrent=lambda: lt.bencode(t.dict))
+
+    def assert_torrent_file(self, tfile:library.TorrentFile, *,
+            info_hash:str=None, start:int=None, stop:int=None, torrent:bytes=None,
+            dummy:tdummy.Torrent=None, dummy_file:Union[tdummy.File, int]=None,
+            mime_type:str=None, content_encoding:str=None, mtime:int=None,
+            filename:str=None, hints:library.Hints=None):
+        if dummy is not None:
+            info_hash = dummy.infohash
+            torrent = lt.bencode(dummy.dict)
+        if dummy_file is not None:
+            if type(dummy_file) is int:
+                assert dummy is not None
+                dummy_file = dummy.files[dummy_file]
+            start = dummy_file.start
+            stop = dummy_file.stop
+            filename = protocol.decode(dummy_file.path_split[-1])
+        assert filename is not None
+        if hints is None:
+            hints = library.Hints(mime_type=mime_type,
+                    content_encoding=content_encoding, mtime=mtime,
+                    filename=filename)
+        assert info_hash is not None
+        assert start is not None
+        assert stop is not None
+        assert torrent is not None
+
+        self.assertEqual(tfile.info_hash, info_hash)
+        self.assertEqual(tfile.start, start)
+        self.assertEqual(tfile.stop, stop)
+        self.assertEqual(tfile.get_torrent(), torrent)
+        self.assertEqual(tfile.hints, hints)
+
+    def assert_is_dir(self, node:fs.Node):
+        self.assertEqual(node.stat().filetype, stat_lib.S_IFDIR)
+
+    def assert_is_regular_file(self, node:fs.Node):
+        self.assertEqual(node.stat().filetype, stat_lib.S_IFREG)
+
+    def assert_is_symlink(self, node:fs.Node):
+        self.assertEqual(node.stat().filetype, stat_lib.S_IFLNK)
+
+    def assert_dirents_like(self, dirents:Iterable[fs.Dirent],
+            expected:Iterable[Tuple[str, Optional[int], str]]):
+        got = [(stat_lib.filemode(d.stat.filetype), d.stat.mtime, d.name) for d in dirents]
+        expected = list(expected)
+        # Test file types only
+        if expected and len(expected[0][0]) == 1:
+            got = [(mode[0], mtime, name) for mode, mtime, name in got]
+        self.assertCountEqual(got, expected)
+
+    def test_get_torrent_path(self):
+        for info_hash in self.torrents:
+            path = self.libs.get_torrent_path(info_hash)
+            torrent_dir = fs.lookup(self.libs.root, path)
+            self.assert_is_dir(torrent_dir)
+
+    def test_lookup_torrent(self):
+        for info_hash in self.torrents:
+            torrent_dir = self.libs.lookup_torrent(info_hash)
+            self.assert_is_dir(torrent_dir)
+
+    def test_get_info_dict_func_fails(self):
+        def whoops(info_hash):
+            raise RuntimeError("whoops")
+        self.libs.get_layout_info_dict_funcs["whoops"] = whoops
+
+        # Test the failer doesn't interfere with torrents known to other
+        # libraries
+        fs.lookup(self.libs.root, f"v1/{SINGLE.infohash}/test/i/0")
+
+        # Ensure it gets called, but correct error is thrown
+        with self.assertRaises(FileNotFoundError):
+            fs.lookup(self.libs.root, f"v1/{'0'*40}")
+
+    def test_get_access_func_fails(self):
+        def whoops(info_hash):
+            raise RuntimeError("whoops")
+        self.libs.get_access_funcs["whoops"] = whoops
+
+        # Test the failer doesn't show up in readdir
+        torrent_dir = fs.lookup(self.libs.root, f"v1/{SINGLE.infohash}")
+        self.assert_dirents_like(torrent_dir.readdir(), [("d", None, "test")])
+
+        # Ensure it gets called, but correct error is thrown
+        with self.assertRaises(FileNotFoundError):
+            fs.lookup(self.libs.root, f"v1/{SINGLE.infohash}/whoops/i/0")
+
+    def test_get_hints_func_fails(self):
+        def whoops(info_hash, index):
+            raise RuntimeError("whoops")
+        self.libs.get_hints_funcs["whoops"] = whoops
+
+        # Test the failer doesn't interfere with other hints
+        tfile = cast(library.TorrentFile, fs.lookup(self.libs.root,
+            f"v1/{SINGLE.infohash}/test/i/0"))
+        self.assert_torrent_file(tfile, dummy=SINGLE,
+                dummy_file=0, mime_type="text/plain")
+
+    def test_hints_with_mtime(self):
+        def get_mtime(info_hash, index):
+            return library.Hints(mtime=12345)
+        self.libs.get_hints_funcs["mtime"] = get_mtime
+
+        tfile = cast(library.TorrentFile, fs.lookup(self.libs.root,
+            f"v1/{SINGLE.infohash}/test/i/0"))
+        self.assertEqual(tfile.hints.mtime, 12345)
+        self.assertEqual(tfile.stat().mtime, 12345)
+
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root, f"v1/{SINGLE.infohash}/test/i"))
+        self.assert_dirents_like(by_index.readdir(), [("-", 12345, "0")])
+
+    def test_browse(self):
+        test_dir = fs.StaticDir()
+        test_dir.mkchild("single", fs.Symlink(target=self.libs.lookup_torrent(
+            SINGLE.infohash)))
+        self.libs.browse_nodes["test"] = test_dir
+
+        browse = fs.lookup(self.libs.root, "browse")
+        self.assert_dirents_like(browse.readdir(), [("d", None, "test")])
+
+        test_dir = cast(fs.Dir, browse.lookup("test"))
+        self.assert_is_dir(test_dir)
+
+        link = cast(fs.Symlink, fs.lookup(self.libs.root,
+            "browse/test/single"))
+        self.assertEqual(str(link.readlink()), f"../../v1/{SINGLE.infohash}")
+
+    def test_v1_lookup(self):
+        for info_hash in self.torrents:
+            self.assert_is_dir(fs.lookup(self.libs.root, f"v1/{info_hash}"))
+
+    def test_v1_lookup_bad(self):
+        v1 = fs.lookup(self.libs.root, "v1")
+        with self.assertRaises(FileNotFoundError):
+            v1.lookup("0" * 40)
+
+    def test_v1_readdir(self):
+        v1 = fs.lookup(self.libs.root, "v1")
+        with self.assertRaises(OSError):
+            list(v1.readdir())
+
+    def test_torrent_dir_readdir(self):
+        for info_hash in self.torrents:
+            torrent_dir = cast(fs.Dir, fs.lookup(self.libs.root, f"v1/{info_hash}"))
+            self.assert_dirents_like(torrent_dir.readdir(), [("d", None,
+                "test")])
+
+    def test_torrent_dir_lookup(self):
+        for info_hash in self.torrents:
+            self.assert_is_dir(fs.lookup(self.libs.root,
+                f"v1/{info_hash}/test"))
+
+    def test_torrent_dir_lookup_bad(self):
+        for info_hash in self.torrents:
+            with self.assertRaises(FileNotFoundError):
+                self.assert_is_dir(fs.lookup(self.libs.root,
+                    f"v1/{info_hash}/does-not-exist"))
+
+    def test_torrent_dir_with_no_access(self):
+        dummy = tdummy.Torrent.single_file(name=b"test.txt", length=100)
+        def get_info_dict(info_hash):
+            return {dummy.infohash: dummy}[info_hash]
+        self.libs.get_layout_info_dict_funcs["dummy"] = get_info_dict
+
+        torrent_dir = fs.lookup(self.libs.root, f"v1/{dummy.infohash}")
+        self.assert_dirents_like(torrent_dir.readdir(), [])
+
+    def test_torrent_dir_with_redirect_access(self):
+        def get_redirect(info_hash):
+            return library.Access(redirect_to="test")
+        self.libs.get_access_funcs["redirect"] = get_redirect
+
+        torrent_dir = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{SINGLE.infohash}"))
+        self.assert_dirents_like(torrent_dir.readdir(), [("l", None,
+            "redirect"), ("d", None, "test")])
+
+        link = cast(fs.Symlink, torrent_dir.lookup("redirect"))
+        self.assertEqual(str(link.readlink()), "test")
+
+    def test_access_readdir(self):
+        for info_hash in self.torrents:
+            access = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{info_hash}/test"))
+            self.assert_dirents_like(access.readdir(), [("d", None, "f"), ("d",
+                None, "i")])
+
+    def test_access_lookup(self):
+        for info_hash in self.torrents:
+            self.assert_is_dir(fs.lookup(self.libs.root, f"v1/{info_hash}/test/f"))
+            self.assert_is_dir(fs.lookup(self.libs.root, f"v1/{info_hash}/test/i"))
+
+    def test_by_path_single(self):
+        by_path = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{SINGLE.infohash}/test/f"))
+
+        self.assert_dirents_like(by_path.readdir(), [("l", None, "test.txt")])
+
+        link = cast(fs.Symlink, by_path.lookup("test.txt"))
+        self.assert_is_symlink(link)
+        self.assertEqual(str(link.readlink()), "../i/0")
+
+    def test_by_index_single(self):
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{SINGLE.infohash}/test/i"))
+
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0")])
+
+        tfile = cast(library.TorrentFile, by_index.lookup("0"))
+        self.assert_torrent_file(tfile, dummy=SINGLE,
+                dummy_file=0, mime_type="text/plain")
+
+    def test_by_path_multi(self):
+        by_path = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{MULTI.infohash}/test/f"))
+
+        self.assert_dirents_like(by_path.readdir(), [("d", None, "multi")])
+
+        subdir = cast(fs.Dir, by_path.lookup("multi"))
+        self.assert_is_dir(subdir)
+
+        self.assert_dirents_like(subdir.readdir(), [("l", None, "file.tar.gz"),
+            ("l", None, "info.nfo")])
+
+        link = cast(fs.Symlink, subdir.lookup("file.tar.gz"))
+        self.assert_is_symlink(link)
+        self.assertEqual(str(link.readlink()), "../../i/0")
+
+        link = cast(fs.Symlink, subdir.lookup("info.nfo"))
+        self.assert_is_symlink(link)
+        self.assertEqual(str(link.readlink()), "../../i/1")
+
+    def test_by_index_multi(self):
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{MULTI.infohash}/test/i"))
+
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0"), ("-",
+            None, "1")])
+
+        tfile = cast(library.TorrentFile, by_index.lookup("0"))
+        self.assert_torrent_file(tfile, dummy=MULTI,
+                dummy_file=0, mime_type="application/x-tar",
+                content_encoding="gzip")
+
+        tfile = cast(library.TorrentFile, by_index.lookup("1"))
+        self.assert_torrent_file(tfile, dummy=MULTI,
+                dummy_file=1, mime_type="text/plain")
+
+    def test_conflict_file(self):
+        # Don't test by-path directory, as its contents are undefined. Do test
+        # that the by-index path still holds file references.
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{CONFLICT_FILE.infohash}/test/i"))
+
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0"), ("-",
+            None, "1")])
+
+        for i in range(len(CONFLICT_FILE.files)):
+            tfile = cast(library.TorrentFile, by_index.lookup(str(i)))
+            self.assert_torrent_file(tfile, dummy=CONFLICT_FILE,
+                    dummy_file=i)
+
+    def test_conflict_file_dir(self):
+        # Don't test by-path directory, as its contents are undefined. Do test
+        # that the by-index path still holds file references.
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{CONFLICT_FILE_DIR.infohash}/test/i"))
+
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0"), ("-",
+            None, "1")])
+
+        for i in range(len(CONFLICT_FILE_DIR.files)):
+            tfile = cast(library.TorrentFile, by_index.lookup(str(i)))
+            self.assert_torrent_file(tfile, dummy=CONFLICT_FILE_DIR,
+                    dummy_file=i)
+
+    def test_conflict_dir_file(self):
+        # Don't test by-path directory, as its contents are undefined. Do test
+        # that the by-index path still holds file references.
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{CONFLICT_DIR_FILE.infohash}/test/i"))
+
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0"), ("-",
+            None, "1")])
+
+        for i in range(len(CONFLICT_DIR_FILE.files)):
+            tfile = cast(library.TorrentFile, by_index.lookup(str(i)))
+            self.assert_torrent_file(tfile, dummy=CONFLICT_DIR_FILE,
+                    dummy_file=i)
+
+    def test_bad_paths(self):
+        # All paths in BAD_PATHS are bad, so the by-path directory should be
+        # empty.
+        by_path = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{BAD_PATHS.infohash}/test/f"))
+        self.assert_dirents_like(by_path.readdir(), [])
+
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{BAD_PATHS.infohash}/test/i"))
+
+        # Ensure we can still access files by index.
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0"), ("-",
+            None, "1"), ("-", None, "2")])
+
+        for i in range(len(BAD_PATHS.files)):
+            tfile = cast(library.TorrentFile, by_index.lookup(str(i)))
+            self.assert_torrent_file(tfile, dummy=BAD_PATHS,
+                    dummy_file=i)
+
+    def test_padded(self):
+        by_path = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{PADDED.infohash}/test/f/padded"))
+        self.assert_dirents_like(by_path.readdir(), [("l", None,
+            "file.tar.gz"), ("l", None, "info.nfo")])
+
+        by_index = cast(fs.Dir, fs.lookup(self.libs.root,
+            f"v1/{PADDED.infohash}/test/i"))
+
+        # Ensure we can still access files by index.
+        self.assert_dirents_like(by_index.readdir(), [("-", None, "0"), ("-", None, "2")])
+
+        for i in range(len(PADDED.files)):
+            if b"p" in PADDED.files[i].attr:
+                with self.assertRaises(FileNotFoundError):
+                    by_index.lookup(str(i))
+            else:
+                tfile = cast(library.TorrentFile, by_index.lookup(str(i)))
+                self.assert_torrent_file(tfile, dummy=PADDED,
+                        dummy_file=i)

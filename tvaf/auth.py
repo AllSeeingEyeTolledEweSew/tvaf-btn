5ea91ddb4904ffa29c4c5a51477a926dd9df80f4
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2020-06-24 07:33:32 +0000

    Add AuthService.

---
+++ tvaf/auth.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import contextlib
+from typing import ContextManager
+from typing import Optional
+import threading
+
+class Error(Exception):
+
+    pass
+
+
+class AuthenticationFailed(Error):
+    pass
+
+
+class _UserContext(contextlib.AbstractContextManager):
+
+    def __init__(self, auth_service:AuthService):
+        self.auth_service = auth_service
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.auth_service.pop_user()
+
+
+class AuthService:
+
+    USER = "tvaf"
+    PASSWORD = "U15OwvGt"
+
+    def __init__(self):
+        self._local = threading.local()
+
+    def auth_password_plain(self, user:str, password:str):
+        if (user, password) == (self.USER, self.PASSWORD):
+            return
+        raise AuthenticationFailed()
+
+    def push_user(self, user:str) -> ContextManager[None]:
+        assert self.get_user() is None
+        ctx = _UserContext(self)
+        self._local.user = user
+        return ctx
+
+    def get_user(self) -> Optional[str]:
+        return getattr(self._local, "user", None)
+
+    def pop_user(self):
+        assert self.get_user() is not None
+        self._local.user = None
---
+++ tvaf/tests/auth_test.py
@@ -0,0 +1,46 @@
+from tvaf import auth
+import unittest
+import concurrent.futures
+
+class TestAuthService(unittest.TestCase):
+
+    def setUp(self):
+        self.auth_service = auth.AuthService()
+
+    def test_auth_good(self):
+        self.auth_service.auth_password_plain(self.auth_service.USER,
+                self.auth_service.PASSWORD)
+
+    def test_auth_bad(self):
+        with self.assertRaises(auth.AuthenticationFailed):
+            self.auth_service.auth_password_plain("invalid", "invalid")
+
+    def test_push_get_pop(self):
+        self.assertEqual(self.auth_service.get_user(), None)
+        self.auth_service.push_user("username")
+        self.assertEqual(self.auth_service.get_user(), "username")
+        self.auth_service.pop_user()
+        self.assertEqual(self.auth_service.get_user(), None)
+
+    def test_push_contextmanager(self):
+        self.assertEqual(self.auth_service.get_user(), None)
+        with self.auth_service.push_user("username"):
+            self.assertEqual(self.auth_service.get_user(), "username")
+        self.assertEqual(self.auth_service.get_user(), None)
+
+    def test_threading(self):
+        def run_test(username):
+            self.assertEqual(self.auth_service.get_user(), None)
+            with self.auth_service.push_user(username):
+                self.assertEqual(self.auth_service.get_user(), username)
+            self.assertEqual(self.auth_service.get_user(), None)
+
+        self.assertEqual(self.auth_service.get_user(), None)
+        with self.auth_service.push_user("main_thread_user"):
+            self.assertEqual(self.auth_service.get_user(), "main_thread_user")
+
+            # Use executor to propagate exceptions
+            executor = concurrent.futures.ThreadPoolExecutor()
+            executor.submit(run_test, "second_thread_user").result()
+        self.assertEqual(self.auth_service.get_user(), None)
+

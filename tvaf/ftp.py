6a202b5cbf102b15377cf44758a94f7166838ee2
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2020-06-24 07:33:56 +0000

    Add FTP server.

---
+++ tvaf/ftp.py
@@ -0,0 +1,284 @@
+from tvaf import fs
+import time
+import logging
+import threading
+import os
+from typing import Mapping
+from typing import Any
+from tvaf import library
+from tvaf import auth
+import io
+from typing import List
+from typing import cast
+import stat as stat_lib
+import errno
+import functools
+import pyftpdlib
+import pyftpdlib.filesystems
+import pyftpdlib.servers
+import pyftpdlib.authorizers
+import pyftpdlib.handlers
+from typing import Callable
+
+
+GetConfig = Callable[[], Mapping[str, Any]]
+
+
+def _partialclass(cls, *args, **kwds):
+    class Wrapped(cls):
+        __init__ = functools.partialmethod(cls.__init__, *args, **kwds)
+    return Wrapped
+
+
+class _FS(pyftpdlib.filesystems.AbstractedFS):
+
+    def __init__(self, *args, root:fs.Dir=None, **kwargs):
+        assert root is not None
+        super().__init__(*args, **kwargs)
+        self.cur_dir = root
+
+    def validpath(self, path:str) -> bool:
+        # This is used to check whether a path traverses symlinks to escape a
+        # home directory.
+        return True
+
+    def ftp2fs(self, ftppath:str) -> str:
+        return self.ftpnorm(ftppath)
+
+    def fs2ftp(self, fspath:str) -> str:
+        return fspath
+
+    def mkstemp(self, *args, **kwargs):
+        raise fs.mkoserror(errno.EROFS)
+
+    def mkdir(self, path:str):
+        raise fs.mkoserror(errno.EROFS)
+
+    def rmdir(self, path:str):
+        raise fs.mkoserror(errno.EROFS)
+
+    def rename(self, src:str, dst:str):
+        raise fs.mkoserror(errno.EROFS)
+
+    def chmod(self, path:str, mode:str):
+        raise fs.mkoserror(errno.EROFS)
+
+    def utime(self, path:str, timeval):
+        raise fs.mkoserror(errno.EROFS)
+
+    def get_user_by_uid(self, uid:int) -> str:
+        return "root"
+
+    def get_group_by_gid(self, gid:int) -> str:
+        return "root"
+
+    def _traverse(self, path:str, *, follow_symlinks=True) -> fs.Node:
+        return self.cur_dir.traverse(path)
+
+    def _ltraverse(self, path:str) -> fs.Node:
+        return self.cur_dir.traverse(path, follow_symlinks=False)
+
+    def _traverse_to_dir(self, path:str) -> fs.Dir:
+        dir_ = cast(fs.Dir, self._traverse(path))
+        if not dir_.is_dir():
+            raise fs.mkoserror(errno.ENOTDIR)
+        return dir_
+
+    def _traverse_to_link(self, path:str) -> fs.Symlink:
+        symlink = cast(fs.Symlink, self._ltraverse(path))
+        if not symlink.is_link():
+            raise fs.mkoserror(errno.EINVAL)
+        return symlink
+
+    def chdir(self, path:str):
+        self.cur_dir = self._traverse_to_dir(path)
+        self.cwd = str(self.cur_dir.abspath())
+
+    def open(self, path:str, mode:str) -> io.RawIOBase:
+        f = cast(fs.File, self._traverse(path))
+        if f.is_dir():
+            raise fs.mkoserror(errno.EISDIR)
+        fp = f.open(mode)
+        return fp
+
+    def listdir(self, path:str) -> List[str]:
+        dir_ = self._traverse_to_dir(path)
+        return [d.name for d in dir_.readdir()]
+
+    def listdirinfo(self, path:str) -> List[str]:
+        # Doesn't seem to be used. However, the base class implements it and we
+        # don't want to allow access to the filesystem.
+        return self.listdir(path)
+
+    def stat(self, path:str) -> os.stat_result:
+        return self._traverse(path).stat().os()
+
+    def lstat(self, path:str) -> os.stat_result:
+        return self._ltraverse(path).stat().os()
+
+    def readlink(self, path:str) -> str:
+        return str(self._traverse_to_link(path).readlink())
+
+    def isfile(self, path:str) -> bool:
+        try:
+            return self._traverse(path).is_file()
+        except OSError:
+            return False
+
+    def islink(self, path:str) -> bool:
+        try:
+            return self._ltraverse(path).is_link()
+        except OSError:
+            return False
+
+    def lexists(self, path:str) -> bool:
+        try:
+            self._ltraverse(path)
+        except OSError:
+            return False
+        return True
+
+    def isdir(self, path:str) -> bool:
+        try:
+            return self._traverse(path).is_dir()
+        except OSError:
+            return False
+
+    def getsize(self, path:str) -> int:
+        return self._traverse(path).stat().size
+
+    def getmtime(self, path:str) -> int:
+        mtime = self._traverse(path).stat().mtime
+        if mtime is not None:
+            return mtime
+        else:
+            return int(time.time())
+
+    def realpath(self, path:str) -> str:
+        return str(self.cur_dir.realpath(path))
+
+
+class _Authorizer(pyftpdlib.authorizers.DummyAuthorizer):
+
+    def __init__(self, *, auth_service:auth.AuthService=None):
+        assert auth_service is not None
+        self.auth_service = auth_service
+
+    def add_user(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def add_anonymous(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def remove_user(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def override_perm(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def has_user(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def get_msg_login(self, username:str) -> str:
+        return "Login successful."
+
+    def get_msg_quit(self, username:str) -> str:
+        return "Goodbye."
+
+    def get_home_dir(self, username:str) -> str:
+        return "/"
+
+    def has_perm(self, username:str, perm:str, path:str=None) -> bool:
+        return perm in self.read_perms
+
+    def get_perms(self, username:str) -> str:
+        return self.read_perms
+
+    def validate_authentication(self, username:str, password:str, handler):
+        try:
+            self.auth_service.auth_password_plain(username, password)
+        except auth.AuthenticationFailed as e:
+            raise pyftpdlib.authorizers.AuthenticationFailed(e)
+
+    def impersonate_user(self, username:str, password:str):
+        self.auth_service.push_user(username)
+
+    def terminate_impersonation(self, username:str):
+        self.auth_service.pop_user()
+
+
+class _FTPHandler(pyftpdlib.handlers.FTPHandler):
+
+    # pyftpd just tests for existence of fileno, but BytesIO and
+    # BufferedTorrentIO expose fileno that raises io.UnsupportedOperation.
+    use_sendfile = False
+
+    def __init__(self, *args, root:fs.Dir=None,
+            auth_service:auth.AuthService=None, **kwargs):
+        assert root is not None
+        assert auth_service is not None
+        super().__init__(*args, **kwargs)
+        self.authorizer = _Authorizer(auth_service=auth_service)
+        self.abstracted_fs = _partialclass(_FS, root=root)
+
+
+class FTPD:
+
+    def __init__(self, *, get_config:GetConfig=None, root:fs.Dir=None, auth_service:auth.AuthService=None):
+        assert auth_service is not None
+        assert root is not None
+        assert get_config is not None
+
+        self.auth_service = auth_service
+        self.root = root
+        self.get_config = get_config
+
+        self._lock = threading.RLock()
+        self.server:Optional[pyftpdlib.servers.FTPServer] = None
+        self.thread:Optional[threading.Thread] = None
+        self._address = ("localhost", 0)  # Dummy value
+
+        self.reload()
+
+    def reload(self):
+        config = self.get_config()
+
+        with self._lock:
+            enabled = bool(config.get("ftp_enabled"))
+            # Only parse address and port if ftp_enabled is true
+            if enabled:
+                address = (config["ftp_bind_address"], int(config["ftp_port"]))
+            else:
+                address = None
+
+            # If our config didn't change, do nothing
+            if enabled == (self.server is not None) and address == self._address:
+                return
+
+            # Our config changed; shut everything down.
+            self.abort()
+            self.wait()
+            if not enabled:
+                return
+
+            self._address = address
+
+            handler = _partialclass(_FTPHandler, root=self.root,
+                    auth_service=self.auth_service)
+            self.server = pyftpdlib.servers.ThreadedFTPServer(address, handler)
+
+            self.thread = threading.Thread(name="ftpd",
+                    target=self.server.serve_forever)
+            self.thread.start()
+
+    def abort(self):
+        with self._lock:
+            if self.server is not None:
+                self.server.close_all()
+
+    def wait(self):
+        with self._lock:
+            if self.thread is not None:
+                self.thread.join()
+            self.thread = None
+            self.server = None
---
+++ tvaf/tests/ftp_test.py
@@ -0,0 +1,225 @@
+from tvaf import ftp
+import io
+import logging
+import ftplib
+import libtorrent as lt
+from tvaf import auth
+from . import tdummy
+from tvaf import types
+from tvaf import library
+import unittest
+
+SINGLE = tdummy.Torrent.single_file(name=b"test.txt", length=16384 * 9 + 1000)
+MULTI = tdummy.Torrent(files=[
+    dict(length=10000, path=b"multi/file.tar.gz"),
+    dict(length=100, path=b"multi/info.nfo"),
+])
+
+class BaseFTPTest(unittest.TestCase):
+
+    do_login = True
+
+    def setUp(self):
+        self.torrents = {t.infohash: t for t in (SINGLE,MULTI)}
+
+        def opener(ref:types.TorrentRef,
+                get_torrent:library.GetTorrent):
+            data = self.torrents[ref.info_hash].data[ref.start:ref.stop]
+            raw = io.BytesIO(data)
+            # bug in pyftpdlib: tries to access fileobj.name for debug logging
+            raw.name = "<bytes>"
+            # opener can normally return a RawIOBase, but we'll mimic
+            # IOService returning BufferedTorrentIO here.
+            return io.BufferedReader(raw)
+
+        def get_access(info_hash):
+            t = self.torrents[info_hash]
+            return library.Access(seeders=100, get_torrent=lambda:
+                    lt.bencode(t.dict))
+
+        self.hints = {
+            (SINGLE.infohash, 0): library.Hints(mime_type="text/plain",
+                mtime=12345),
+        }
+
+        self.libs = library.LibraryService(opener=opener)
+        self.libs.get_layout_info_dict_funcs["test"] = lambda info_hash: self.torrents[info_hash].info
+        self.libs.get_access_funcs["test"] = get_access
+        self.libs.get_hints_funcs["test"] = lambda ih, idx: self.hints[(ih, idx)]
+
+        self.auth_service = auth.AuthService()
+        self.config = dict(ftp_enabled=True, ftp_bind_address="localhost",
+                ftp_port=0)
+        self.ftpd = ftp.FTPD(get_config=lambda: self.config, root=self.libs.root, auth_service=self.auth_service)
+        self.address = self.ftpd.server.socket.getsockname()
+        self.connect()
+
+    def connect(self):
+        # Can't seem to specify a port with constructor args
+        self.ftp = ftplib.FTP()
+        self.ftp.set_debuglevel(2)
+        self.ftp.connect(host=self.address[0], port=self.address[1], timeout=5)
+        if self.do_login:
+            self.ftp.login(user=self.auth_service.USER,
+                    passwd=self.auth_service.PASSWORD)
+
+    def tearDown(self):
+        self.ftp.quit()
+        self.ftpd.abort()
+        self.ftpd.wait()
+
+
+class TestPathStructure(BaseFTPTest):
+
+    def assert_mlsd(self, path, facts, expected):
+        items = sorted(self.ftp.mlsd(path=path, facts=facts))
+        self.assertEqual(items, expected)
+
+    def test_LIST(self):
+        lines = []
+        self.ftp.retrlines("LIST", callback=lines.append)
+        # The last token should be the name
+        lines = sorted(line.split()[-1] for line in lines)
+        self.assertEqual(lines, ["browse", "v1"])
+
+    def test_LIST_with_symlinks(self):
+        self.ftp.cwd(f"/v1/{SINGLE.infohash}/test/f")
+        lines = []
+        self.ftp.retrlines("LIST", callback=lines.append)
+        self.assertEqual(len(lines), 1)
+        self.assertTrue(lines[0].endswith(" test.txt -> ../i/0"))
+
+    def test_root(self):
+        self.assertEqual(self.ftp.pwd(), "/")
+
+        self.assert_mlsd("", ("perm", "type"), [("browse", dict(perm="el", type="dir")),
+            ("v1", dict(perm="el", type="dir"))])
+
+    def test_torrent_dir(self):
+        self.ftp.cwd(f"/v1/{SINGLE.infohash}/test/f")
+        self.assert_mlsd("", ("perm", "size", "type"), [("test.txt", dict(perm="r",
+            type="file", size=str(SINGLE.files[0].length)))])
+
+        self.ftp.cwd(f"/v1/{SINGLE.infohash}/test/i")
+        self.assert_mlsd("", ("perm", "size", "type"), [("0", dict(perm="r",
+            type="file", size=str(SINGLE.files[0].length)))])
+
+    def test_CWD_invalid(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 Not a directory."):
+            self.ftp.cwd(f"/v1/{SINGLE.infohash}/test/i/0")
+
+    def test_SIZE(self):
+        # pyftpdlib does not allow SIZE in ascii mode to avoid newline
+        # translation issues
+        self.ftp.voidcmd("TYPE I")
+        size = self.ftp.size(f"/v1/{SINGLE.infohash}/test/i/0")
+        self.assertEqual(size, SINGLE.files[0].length)
+
+    def test_MDTM(self):
+        resp = self.ftp.sendcmd(f"MDTM /v1/{SINGLE.infohash}/test/i/0")
+        self.assertEqual(resp, "213 19700101032545")
+
+        # Check files which default to current mtime
+        resp = self.ftp.sendcmd(f"MDTM /v1/{MULTI.infohash}/test/i/0")
+        self.assertEqual(resp.split()[0], "213")
+
+
+class TestReadOnly(BaseFTPTest):
+
+    def test_STOU(self):
+        # For some reason, pyftpdlib bypasses permissions to call mkstemp. tvaf
+        # fails with a permissions error, but pyftpdlib treats it as a 450
+        # "temporary error".
+        with self.assertRaises(ftplib.all_errors):
+            self.ftp.storbinary("STOU", io.BytesIO(b"data"))
+
+    def test_STOR(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.storbinary("STOR file.txt", io.BytesIO(b"data"))
+
+    def test_STOR_overwrite(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.storbinary(f"STOR /v1/{SINGLE.infohash}/test/i/0",
+                    io.BytesIO(b"data"))
+
+    def test_APPE(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.storbinary(f"APPE /v1/{SINGLE.infohash}/test/i/0",
+                    io.BytesIO(b"data"))
+
+    def test_DELE(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.delete(f"/v1/{SINGLE.infohash}/test/i/0")
+
+    def test_rename(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.rename(f"/v1/{SINGLE.infohash}/test/i/0",
+                    f"/v1/{SINGLE.infohash}/test/i/123")
+
+    def test_MKD(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.mkd("new")
+
+    def test_RMD(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.rmd("v1")
+
+    def test_MFMT(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.voidcmd(f"MFMT 19700101032545 /v1/{SINGLE.infohash}/test/i/0")
+
+
+class TestRETR(BaseFTPTest):
+
+    def test_RETR(self):
+        buf = io.BytesIO()
+        self.ftp.retrbinary(f"RETR /v1/{SINGLE.infohash}/test/i/0", buf.write)
+        self.assertEqual(buf.getvalue(), SINGLE.files[0].data)
+
+    def test_RETR_dir(self):
+        buf = io.BytesIO()
+        with self.assertRaisesRegex(ftplib.error_perm, "550 .*"):
+            self.ftp.retrbinary(f"RETR /v1/{SINGLE.infohash}/test/i", buf.write)
+
+    def test_RETR_with_REST(self):
+        buf = io.BytesIO()
+        self.ftp.retrbinary(f"RETR /v1/{SINGLE.infohash}/test/i/0", buf.write,
+                rest=1000)
+        self.assertEqual(buf.getvalue(), SINGLE.files[0].data[1000:])
+
+
+class TestAuth(BaseFTPTest):
+
+    do_login = False
+
+    def test_bad_auth(self):
+        with self.assertRaisesRegex(ftplib.error_perm, "530 .*"):
+            self.ftp.login(user="invalid", passwd="invalid")
+
+
+class TestConfig(BaseFTPTest):
+
+    def test_change_binding(self):
+        self.config["ftp_bind_address"] = "127.0.0.1"
+        self.ftpd.reload()
+        with self.assertRaises(EOFError):
+            self.ftp.pwd()
+        self.address = self.ftpd.server.socket.getsockname()
+        self.connect()
+        self.assertEqual(self.ftp.pwd(), "/")
+
+    def test_disable_enable(self):
+        self.config["ftp_enabled"] = False
+        self.ftpd.reload()
+
+        with self.assertRaises(EOFError):
+            self.ftp.pwd()
+
+        self.assertIsNone(self.ftpd.server)
+
+        self.config["ftp_enabled"] = True
+        self.ftpd.reload()
+
+        self.address = self.ftpd.server.socket.getsockname()
+        self.connect()
+        self.assertEqual(self.ftp.pwd(), "/")

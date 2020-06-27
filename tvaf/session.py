2fe7c5eb6d3c0115bb7b5aced599a087e1ecf5f4
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2020-06-27 08:12:11 +0000

    Add SessionService.

---
+++ tvaf/session.py
@@ -0,0 +1,86 @@
+import libtorrent as lt
+import contextlib
+from tvaf import config as config_lib
+from typing import Callable
+import threading
+from typing import Dict
+from typing import Any
+from tvaf import ltpy
+
+_OVERRIDES = {
+        "announce_ip": "",
+        "handshake_client_version": "",
+        "enable_lsd": False,
+        "enable_dht": False,
+        "alert_queue_size": 2 ** 32 - 1,
+    }
+
+_BLACKLIST = {
+        "user_agent",
+        "peer_fingerprint",
+    }
+
+@contextlib.contextmanager
+def _translate_exceptions():
+    try:
+        with ltpy.translate_exceptions():
+            yield
+    except (KeyError, TypeError, ltpy.Error) as exc:
+        raise config_lib.InvalidConfigError(str(exc)) from exc
+
+class SessionService:
+
+    def __init__(self, *, get_required_alert_mask:Callable[[], int]=None, config:config_lib.Config=None):
+        assert get_required_alert_mask is not None
+        assert config is not None
+
+        self._lock = threading.RLock()
+        self.get_required_alert_mask = get_required_alert_mask
+
+        with _translate_exceptions():
+            self._settings = self._parse_config(config)
+            self.session = lt.session(self._settings)
+
+    def _parse_config(self, config:config_lib.Config) -> Dict[str, Any]:
+        config.setdefault("session_settings_base", "default_settings")
+
+        settings_base_name = config.require_str("session_settings_base")
+        if settings_base_name not in ("default_settings",
+                "high_performance_seed"):
+            raise config_lib.InvalidConfigError(f"no settings pack named \"{settings_base_name}\"")
+        settings = getattr(lt, settings_base_name)()
+
+        for key, value in config.items():
+            if not key.startswith("session_"):
+                continue
+            key = key[len("session_"):]
+            if key == "settings_base":
+                continue
+
+            if key in _BLACKLIST:
+                continue
+
+            settings[key] = value
+
+        # Update our static overrides
+        settings.update(_OVERRIDES)
+
+        # Specialized override: alert_mask
+        alert_mask = settings.get("alert_mask", 0)
+        if not isinstance(alert_mask, int):
+            raise config_lib.InvalidConfigError(f"alert_mask is {alert_mask!r}, not int")
+        settings["alert_mask"] = alert_mask | self.get_required_alert_mask()
+
+        return settings
+
+    def set_config(self, config:config_lib.Config):
+        with _translate_exceptions():
+            settings = self._parse_config(config)
+            with self._lock:
+                deltas = dict(set(settings.items()) -
+                        set(self._settings.items()))
+                if not deltas:
+                    return
+                # As far as I can tell, apply_settings never partially fails
+                self.session.apply_settings(deltas)
+                self._settings = settings
---
+++ tvaf/tests/session_test.py
@@ -0,0 +1,109 @@
+import unittest
+from tvaf import config as config_lib
+import libtorrent as lt
+from tvaf import session as session_lib
+
+class TestSession(unittest.TestCase):
+
+    def setUp(self):
+        self.config = config_lib.Config(session_listen_interfaces="127.0.0.1:0")
+        self.required_alert_mask = (lt.alert_category.error |
+                lt.alert_category.peer)
+        self.get_required_alert_mask = lambda: self.required_alert_mask
+
+    def create_session_service(self):
+        return session_lib.SessionService(get_required_alert_mask=self.get_required_alert_mask,
+                config=self.config)
+
+    def test_session(self):
+        session_service = self.create_session_service()
+        settings = session_service.session.get_settings()
+
+        # Test required alert mask is applied
+        self.assertEqual(settings["alert_mask"] & self.required_alert_mask,
+                self.required_alert_mask)
+
+        # Test overrides are applied
+        self.assertEqual(settings["enable_dht"], False)
+
+        # Test default config is added
+        self.assertEqual(self.config["session_settings_base"],
+        "default_settings")
+
+    def test_alert_mask(self):
+        self.config["session_alert_mask"] = lt.alert_category.session_log
+
+        session_service = self.create_session_service()
+        settings = session_service.session.get_settings()
+
+        # Test required mask was added
+        self.assertEqual(
+            settings["alert_mask"],
+            lt.alert_category.session_log | self.required_alert_mask)
+
+    def test_overrides(self):
+        self.config["session_enable_dht"] = True
+
+        session_service = self.create_session_service()
+        settings = session_service.session.get_settings()
+
+        # Test overrides are applied
+        self.assertEqual(settings["enable_dht"], False)
+
+    def test_blacklist(self):
+        self.config["session_user_agent"] = "test"
+
+        session_service = self.create_session_service()
+        settings = session_service.session.get_settings()
+
+        # Test blacklisted setting gets replaced by libtorrent default
+        self.assertNotEqual(settings["user_agent"], "test")
+
+    def test_reconfigure(self):
+        session_service = self.create_session_service()
+        settings = session_service.session.get_settings()
+
+        # Sanity check: close_redundant_connections should be True by default
+        self.assertEqual(settings["close_redundant_connections"], True)
+
+        self.config["session_close_redundant_connections"] = False
+        session_service.set_config(self.config)
+
+        settings = session_service.session.get_settings()
+        self.assertEqual(settings["close_redundant_connections"], False)
+
+    def test_reconfigure_no_changes(self):
+        session_service = self.create_session_service()
+        session_service.set_config(self.config)
+
+    def test_settings_base(self):
+        self.config["session_settings_base"] = "high_performance_seed"
+
+        session_service = self.create_session_service()
+        settings = session_service.session.get_settings()
+
+        # Check settings pack was applied as default
+        self.assertEqual(settings["cache_size"],
+                lt.high_performance_seed()["cache_size"])
+
+        # Check base pack name didn't get overwritten
+        self.assertEqual(self.config["session_settings_base"],
+                "high_performance_seed")
+
+    def test_settings_base_invalid(self):
+        self.config["session_settings_base"] = "invalid"
+
+        with self.assertRaises(config_lib.InvalidConfigError):
+            session_service = self.create_session_service()
+
+    def test_setting_invalid_type(self):
+        self.config["session_cache_size"] = "invalid"
+
+        with self.assertRaises(config_lib.InvalidConfigError):
+            session_service = self.create_session_service()
+
+    def test_alert_mask_invalid_type(self):
+        self.config["session_alert_mask"] = "invalid"
+
+        with self.assertRaises(config_lib.InvalidConfigError):
+            session_service = self.create_session_service()

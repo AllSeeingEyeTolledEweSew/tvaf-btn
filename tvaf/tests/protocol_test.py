e359e825776fcaeb6fc048e58d1192b635a32f7e
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2020-07-30 15:32:55 +0000

    Some lint fixes.

--- tvaf/tests/protocol_test.py
+++ tvaf/tests/protocol_test.py
@@ -9,23 +9,23 @@ class IterFilesTest(unittest.TestCase):
         info = protocol.Info({b"name": b"file name \xff.txt", b"length": 10000})
         files = list(info.iter_files())
         self.assertEqual(len(files), 1)
-        f = files[0]
-        self.assertEqual(f.index, 0)
-        self.assertEqual(f.length, 10000)
-        self.assertEqual(f.start, 0)
-        self.assertEqual(f.stop, 10000)
+        file_ = files[0]
+        self.assertEqual(file_.index, 0)
+        self.assertEqual(file_.length, 10000)
+        self.assertEqual(file_.start, 0)
+        self.assertEqual(file_.stop, 10000)
 
-        self.assertEqual(f.path_bytes, [])
-        self.assertEqual(f.path, [])
-        self.assertEqual(f.full_path_bytes, [b"file name \xff.txt"])
-        self.assertEqual(f.full_path, ["file name \udcff.txt"])
+        self.assertEqual(file_.path_bytes, [])
+        self.assertEqual(file_.path, [])
+        self.assertEqual(file_.full_path_bytes, [b"file name \xff.txt"])
+        self.assertEqual(file_.full_path, ["file name \udcff.txt"])
 
-        self.assertEqual(f.attr_bytes, b"")
-        self.assertEqual(f.attr, "")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"")
+        self.assertEqual(file_.attr, "")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
     def test_single_file_attr(self):
         info = protocol.Info({
@@ -35,23 +35,23 @@ class IterFilesTest(unittest.TestCase):
         })
         files = list(info.iter_files())
         self.assertEqual(len(files), 1)
-        f = files[0]
-        self.assertEqual(f.index, 0)
-        self.assertEqual(f.length, 10000)
-        self.assertEqual(f.start, 0)
-        self.assertEqual(f.stop, 10000)
+        file_ = files[0]
+        self.assertEqual(file_.index, 0)
+        self.assertEqual(file_.length, 10000)
+        self.assertEqual(file_.start, 0)
+        self.assertEqual(file_.stop, 10000)
 
-        self.assertEqual(f.path_bytes, [])
-        self.assertEqual(f.path, [])
-        self.assertEqual(f.full_path_bytes, [b"file name \xff.txt"])
-        self.assertEqual(f.full_path, ["file name \udcff.txt"])
+        self.assertEqual(file_.path_bytes, [])
+        self.assertEqual(file_.path, [])
+        self.assertEqual(file_.full_path_bytes, [b"file name \xff.txt"])
+        self.assertEqual(file_.full_path, ["file name \udcff.txt"])
 
-        self.assertEqual(f.attr_bytes, b"hx\xff")
-        self.assertEqual(f.attr, "hx\udcff")
-        self.assertFalse(f.is_symlink)
-        self.assertTrue(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertTrue(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"hx\xff")
+        self.assertEqual(file_.attr, "hx\udcff")
+        self.assertFalse(file_.is_symlink)
+        self.assertTrue(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertTrue(file_.is_executable)
 
     def test_multi_file(self):
         info = protocol.Info({
@@ -70,43 +70,43 @@ class IterFilesTest(unittest.TestCase):
         })
         files = list(info.iter_files())
         self.assertEqual(len(files), 2)
-        f = files[0]
-        self.assertEqual(f.index, 0)
-        self.assertEqual(f.length, 20000)
-        self.assertEqual(f.start, 0)
-        self.assertEqual(f.stop, 20000)
+        file_ = files[0]
+        self.assertEqual(file_.index, 0)
+        self.assertEqual(file_.length, 20000)
+        self.assertEqual(file_.start, 0)
+        self.assertEqual(file_.stop, 20000)
 
-        self.assertEqual(f.path_bytes, [b"directory", b"file.zip"])
-        self.assertEqual(f.path, ["directory", "file.zip"])
-        self.assertEqual(f.full_path_bytes,
+        self.assertEqual(file_.path_bytes, [b"directory", b"file.zip"])
+        self.assertEqual(file_.path, ["directory", "file.zip"])
+        self.assertEqual(file_.full_path_bytes,
                          [b"parent", b"directory", b"file.zip"])
-        self.assertEqual(f.full_path, ["parent", "directory", "file.zip"])
+        self.assertEqual(file_.full_path, ["parent", "directory", "file.zip"])
 
-        self.assertEqual(f.attr_bytes, b"")
-        self.assertEqual(f.attr, "")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"")
+        self.assertEqual(file_.attr, "")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
-        f = files[1]
-        self.assertEqual(f.index, 1)
-        self.assertEqual(f.length, 1000)
-        self.assertEqual(f.start, 20000)
-        self.assertEqual(f.stop, 21000)
+        file_ = files[1]
+        self.assertEqual(file_.index, 1)
+        self.assertEqual(file_.length, 1000)
+        self.assertEqual(file_.start, 20000)
+        self.assertEqual(file_.stop, 21000)
 
-        self.assertEqual(f.path_bytes, [b"directory", b"info.nfo"])
-        self.assertEqual(f.path, ["directory", "info.nfo"])
-        self.assertEqual(f.full_path_bytes,
+        self.assertEqual(file_.path_bytes, [b"directory", b"info.nfo"])
+        self.assertEqual(file_.path, ["directory", "info.nfo"])
+        self.assertEqual(file_.full_path_bytes,
                          [b"parent", b"directory", b"info.nfo"])
-        self.assertEqual(f.full_path, ["parent", "directory", "info.nfo"])
+        self.assertEqual(file_.full_path, ["parent", "directory", "info.nfo"])
 
-        self.assertEqual(f.attr_bytes, b"")
-        self.assertEqual(f.attr, "")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"")
+        self.assertEqual(file_.attr, "")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
     def test_pad(self):
         info = protocol.Info({
@@ -126,42 +126,42 @@ class IterFilesTest(unittest.TestCase):
         })
         files = list(info.iter_files())
         self.assertEqual(len(files), 2)
-        f = files[0]
-        self.assertEqual(f.index, 0)
-        self.assertEqual(f.length, 16000)
-        self.assertEqual(f.start, 0)
-        self.assertEqual(f.stop, 16000)
+        file_ = files[0]
+        self.assertEqual(file_.index, 0)
+        self.assertEqual(file_.length, 16000)
+        self.assertEqual(file_.start, 0)
+        self.assertEqual(file_.stop, 16000)
 
-        self.assertEqual(f.path_bytes, [b"directory", b"file.zip"])
-        self.assertEqual(f.path, ["directory", "file.zip"])
-        self.assertEqual(f.full_path_bytes,
+        self.assertEqual(file_.path_bytes, [b"directory", b"file.zip"])
+        self.assertEqual(file_.path, ["directory", "file.zip"])
+        self.assertEqual(file_.full_path_bytes,
                          [b"parent", b"directory", b"file.zip"])
-        self.assertEqual(f.full_path, ["parent", "directory", "file.zip"])
+        self.assertEqual(file_.full_path, ["parent", "directory", "file.zip"])
 
-        self.assertEqual(f.attr_bytes, b"")
-        self.assertEqual(f.attr, "")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"")
+        self.assertEqual(file_.attr, "")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
-        f = files[1]
-        self.assertEqual(f.index, 1)
-        self.assertEqual(f.length, 384)
-        self.assertEqual(f.start, 16000)
-        self.assertEqual(f.stop, 16384)
+        file_ = files[1]
+        self.assertEqual(file_.index, 1)
+        self.assertEqual(file_.length, 384)
+        self.assertEqual(file_.start, 16000)
+        self.assertEqual(file_.stop, 16384)
 
-        self.assertEqual(f.path_bytes, [b".pad", b"384"])
-        self.assertEqual(f.path, [".pad", "384"])
-        self.assertEqual(f.full_path_bytes, [b"parent", b".pad", b"384"])
-        self.assertEqual(f.full_path, ["parent", ".pad", "384"])
+        self.assertEqual(file_.path_bytes, [b".pad", b"384"])
+        self.assertEqual(file_.path, [".pad", "384"])
+        self.assertEqual(file_.full_path_bytes, [b"parent", b".pad", b"384"])
+        self.assertEqual(file_.full_path, ["parent", ".pad", "384"])
 
-        self.assertEqual(f.attr_bytes, b"p")
-        self.assertEqual(f.attr, "p")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertTrue(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"p")
+        self.assertEqual(file_.attr, "p")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertTrue(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
     def test_multi_file_attr(self):
         info = protocol.Info({
@@ -181,42 +181,43 @@ class IterFilesTest(unittest.TestCase):
         })
         files = list(info.iter_files())
         self.assertEqual(len(files), 2)
-        f = files[0]
-        self.assertEqual(f.index, 0)
-        self.assertEqual(f.length, 20000)
-        self.assertEqual(f.start, 0)
-        self.assertEqual(f.stop, 20000)
+        file_ = files[0]
+        self.assertEqual(file_.index, 0)
+        self.assertEqual(file_.length, 20000)
+        self.assertEqual(file_.start, 0)
+        self.assertEqual(file_.stop, 20000)
 
-        self.assertEqual(f.path_bytes, [b"directory", b"file.zip"])
-        self.assertEqual(f.path, ["directory", "file.zip"])
-        self.assertEqual(f.full_path_bytes,
+        self.assertEqual(file_.path_bytes, [b"directory", b"file.zip"])
+        self.assertEqual(file_.path, ["directory", "file.zip"])
+        self.assertEqual(file_.full_path_bytes,
                          [b"parent", b"directory", b"file.zip"])
-        self.assertEqual(f.full_path, ["parent", "directory", "file.zip"])
+        self.assertEqual(file_.full_path, ["parent", "directory", "file.zip"])
 
-        self.assertEqual(f.attr_bytes, b"")
-        self.assertEqual(f.attr, "")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"")
+        self.assertEqual(file_.attr, "")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
-        f = files[1]
-        self.assertEqual(f.index, 1)
-        self.assertEqual(f.length, 1000)
-        self.assertEqual(f.start, 20000)
-        self.assertEqual(f.stop, 21000)
+        file_ = files[1]
+        self.assertEqual(file_.index, 1)
+        self.assertEqual(file_.length, 1000)
+        self.assertEqual(file_.start, 20000)
+        self.assertEqual(file_.stop, 21000)
 
-        self.assertEqual(f.path_bytes, [b"directory", b".sig"])
-        self.assertEqual(f.path, ["directory", ".sig"])
-        self.assertEqual(f.full_path_bytes, [b"parent", b"directory", b".sig"])
-        self.assertEqual(f.full_path, ["parent", "directory", ".sig"])
+        self.assertEqual(file_.path_bytes, [b"directory", b".sig"])
+        self.assertEqual(file_.path, ["directory", ".sig"])
+        self.assertEqual(file_.full_path_bytes,
+                         [b"parent", b"directory", b".sig"])
+        self.assertEqual(file_.full_path, ["parent", "directory", ".sig"])
 
-        self.assertEqual(f.attr_bytes, b"hx?")
-        self.assertEqual(f.attr, "hx?")
-        self.assertFalse(f.is_symlink)
-        self.assertTrue(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertTrue(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"hx?")
+        self.assertEqual(file_.attr, "hx?")
+        self.assertFalse(file_.is_symlink)
+        self.assertTrue(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertTrue(file_.is_executable)
 
     def test_symlink(self):
         info = protocol.Info({
@@ -236,44 +237,44 @@ class IterFilesTest(unittest.TestCase):
         })
         files = list(info.iter_files())
         self.assertEqual(len(files), 2)
-        f = files[0]
-        self.assertEqual(f.index, 0)
-        self.assertEqual(f.length, 20000)
-        self.assertEqual(f.start, 0)
-        self.assertEqual(f.stop, 20000)
+        file_ = files[0]
+        self.assertEqual(file_.index, 0)
+        self.assertEqual(file_.length, 20000)
+        self.assertEqual(file_.start, 0)
+        self.assertEqual(file_.stop, 20000)
 
-        self.assertEqual(f.path_bytes, [b"directory", b"file.zip"])
-        self.assertEqual(f.path, ["directory", "file.zip"])
-        self.assertEqual(f.full_path_bytes,
+        self.assertEqual(file_.path_bytes, [b"directory", b"file.zip"])
+        self.assertEqual(file_.path, ["directory", "file.zip"])
+        self.assertEqual(file_.full_path_bytes,
                          [b"parent", b"directory", b"file.zip"])
-        self.assertEqual(f.full_path, ["parent", "directory", "file.zip"])
+        self.assertEqual(file_.full_path, ["parent", "directory", "file.zip"])
 
-        self.assertEqual(f.attr_bytes, b"")
-        self.assertEqual(f.attr, "")
-        self.assertFalse(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"")
+        self.assertEqual(file_.attr, "")
+        self.assertFalse(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
 
-        f = files[1]
-        self.assertEqual(f.index, 1)
-        self.assertEqual(f.length, 0)
+        file_ = files[1]
+        self.assertEqual(file_.index, 1)
+        self.assertEqual(file_.length, 0)
 
-        self.assertEqual(f.path_bytes, [b"directory", b"FILE.ZIP"])
-        self.assertEqual(f.path, ["directory", "FILE.ZIP"])
-        self.assertEqual(f.full_path_bytes,
+        self.assertEqual(file_.path_bytes, [b"directory", b"FILE.ZIP"])
+        self.assertEqual(file_.path, ["directory", "FILE.ZIP"])
+        self.assertEqual(file_.full_path_bytes,
                          [b"parent", b"directory", b"FILE.ZIP"])
-        self.assertEqual(f.full_path, ["parent", "directory", "FILE.ZIP"])
+        self.assertEqual(file_.full_path, ["parent", "directory", "FILE.ZIP"])
 
-        self.assertEqual(f.target_bytes, [b"directory", b"file.zip"])
-        self.assertEqual(f.target, ["directory", "file.zip"])
-        self.assertEqual(f.full_target_bytes,
+        self.assertEqual(file_.target_bytes, [b"directory", b"file.zip"])
+        self.assertEqual(file_.target, ["directory", "file.zip"])
+        self.assertEqual(file_.full_target_bytes,
                          [b"parent", b"directory", b"file.zip"])
-        self.assertEqual(f.full_target, ["parent", "directory", "file.zip"])
+        self.assertEqual(file_.full_target, ["parent", "directory", "file.zip"])
 
-        self.assertEqual(f.attr_bytes, b"l")
-        self.assertEqual(f.attr, "l")
-        self.assertTrue(f.is_symlink)
-        self.assertFalse(f.is_hidden)
-        self.assertFalse(f.is_pad)
-        self.assertFalse(f.is_executable)
+        self.assertEqual(file_.attr_bytes, b"l")
+        self.assertEqual(file_.attr, "l")
+        self.assertTrue(file_.is_symlink)
+        self.assertFalse(file_.is_hidden)
+        self.assertFalse(file_.is_pad)
+        self.assertFalse(file_.is_executable)
